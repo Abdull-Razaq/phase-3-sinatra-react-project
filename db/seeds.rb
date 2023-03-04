278892fd01614e9a2500bb436fa4b935a22f24b0
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-03-04 17:19:09 +0300

    Dummy data addition

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# project instance
+project1 = Project.create(
+    project_name: "Portfolio",
+    status: "Incomplete"
+)
+
+# user instance 
+user1 = User.create(
+    username: "Abdull",
+    password: 11223344
+)
+
+# member instance
+member1 = Member.create(
+    user_id: user1.id, 
+    project_id: project1.id
+)
+
+
 
 puts "✅ Done seeding!"
